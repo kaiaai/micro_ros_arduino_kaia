bd3a60e5752ee8d0bbee7a26a2d0439d525e9b61
Ilia O <iliao@remake.ai>
2023-08-07 23:20:28 -0700

    Built lib

---
+++ src/kaia_msgs/msg/kaia_telemetry.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from kaia_msgs:msg/KaiaTelemetry.idl
+// generated code does not contain a copyright notice
+
+#ifndef KAIA_MSGS__MSG__KAIA_TELEMETRY_H_
+#define KAIA_MSGS__MSG__KAIA_TELEMETRY_H_
+
+#include "kaia_msgs/msg/detail/kaia_telemetry__struct.h"
+#include "kaia_msgs/msg/detail/kaia_telemetry__functions.h"
+#include "kaia_msgs/msg/detail/kaia_telemetry__type_support.h"
+
+#endif  // KAIA_MSGS__MSG__KAIA_TELEMETRY_H_
