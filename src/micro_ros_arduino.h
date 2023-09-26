9b08673c7f9b4f7cf2a4dc0cf96d922671ce2a44
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-09-26 15:50:25 -0700

    Transports setup returns error

--- src/micro_ros_arduino.h
+++ src/micro_ros_arduino.h
@@ -150,13 +150,13 @@ static inline void set_microros_wifi_transports(char * ssid, char * pass, char *
 	);
 }
 
-static inline void set_microros_wifi_transports(const char * agent_ip, const uint agent_port){
+static inline int set_microros_wifi_transports(const char * agent_ip, const uint agent_port) {
 
 	static struct micro_ros_agent_locator locator;
 	locator.address.fromString(agent_ip);
 	locator.port = agent_port;
 
-	rmw_uros_set_custom_transport(
+	return rmw_uros_set_custom_transport(
 		false,
 		(void *) &locator,
 		arduino_wifi_transport_open,
