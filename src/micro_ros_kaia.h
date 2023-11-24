a131568399bb78c7ad2a86b53359eec84c92b0ba
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-11-23 23:29:34 -0800

    Update micro_ros_kaia.h

--- src/micro_ros_kaia.h
+++ src/micro_ros_kaia.h
@@ -1,6 +1,17 @@
 #ifndef MICRO_ROS_KAIA
 #define MICRO_ROS_KAIA
 
+#define MICRO_ROS_KAIA_DISTRO humble
+#define MICRO_ROS_KAIA_VERSION_MAJOR 2
+#define MICRO_ROS_KAIA_VERSION_MINOR 0
+#define MICRO_ROS_KAIA_VERSION_PATCH 7
+#define MICRO_ROS_KAIA_VERSION_BUILD 3
+#define IS_MICRO_ROS_KAIA_MIN_VERSION(major,minor,patch,build) ( \
+  (MICRO_ROS_KAIA_VERSION_MAJOR >= major) && \
+  (MICRO_ROS_KAIA_VERSION_MINOR >= minor) && \
+  (MICRO_ROS_KAIA_VERSION_PATCH >= patch) && \
+  (MICRO_ROS_KAIA_VERSION_BUILD >= build))
+
 #include <micro_ros_arduino.h>
 
 #endif
